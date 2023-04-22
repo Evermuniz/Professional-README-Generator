a8096e14141dfa6e24ecf3ec18c9a20011abc4ab
evermuniz <evermuniz@outlook.com>
2023-04-21 19:39:36 -0600

    added rest of questions

--- index.js
+++ index.js
@@ -6,16 +6,52 @@ const generateMarkdown = require('./generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = [
   {
-    type: "input",
-    name: "title",
-    message: "What is the title of your project?",
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
   },
   {
-    type: "list",
-    name: "license",
-    message: "Which license would you like to use for your project?",
-    choices: ["MIT", "Apache 2.0", 'GNU GPLv3'],
+    type: 'list',
+    name: 'license',
+    message: 'Which license would you like to use for your project?',
+    choices: ['MIT', 'Apache 2.0', 'GNU GPLv3'],
   },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a short description explaining the what, why, and how of your project.',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide instructions and examples for use.'
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'If you would like other developers to contribute to your application, you can include guidelines for how to do so.'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Write tests for your application then provide examples on how to run them here.'
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub URL?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is the best email address users can use to contact you?'
+  },
+
 ];
 
 // TODO: Create a function to write README file
