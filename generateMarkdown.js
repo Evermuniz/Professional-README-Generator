3b258ab24d0f95cbb68995e4669daa758a90912b
evermuniz <evermuniz@outlook.com>
2023-04-21 18:44:36 -0600

    license functions and README template

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -21,15 +21,65 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-//function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+   if (license !== "None") {
+     return `\n* [License](#license)\n`;
+   }
+   return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-//function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license  !== 'None') {
+    return `## License
+    This project is licensed under the ${license} license. `
+  }
+  return '';
+}
+
+const licenseBadge = renderLicenseBadge(license);
+const licenseLink = renderLicenseLink(license);
+const licenseSection = renderLicenseSection;
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title} ${license}`;
+  return `# ${data.title} ${licenseBadge}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - ${licenseLink}
+  - [Questions](#questions)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Credits
+
+  ${data.credits}
+
+  ## License
+
+  ${licenseSection}
+
+
+  ## Questions
+
+  If you have any questions, please contact me at ${data.email}.
+  You can also visit my GitHub profile: https://github.com/${data.username}/
+  `;
   
 }
 
--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Which license would you like to use for your project?',
-        choices: ['MIT', 'Apache 2.0', 'GPL']
-    },
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Which license would you like to use for your project?",
+    choices: ["MIT", "Apache 2.0", 'GNU GPLv3'],
+  },
 ];
 
 // TODO: Create a function to write README file
@@ -24,7 +24,13 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        console.log(answers);
+    });
+}
 
 // Function call to initialize app
-init();
+init() 
+    
+
