4cd91b6a72af99c117b5b46d962ad0a3766d43d1
evermuniz <evermuniz@outlook.com>
2023-04-21 18:14:49 -0600

    lots of trial and error/added questions

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license would you like to use for your project?',
+        choices: ['MIT', 'Apache 2.0', 'GPL']
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
